c7cc082ef8d9676b3b82a27e416c845e52853eb1
Alexander Lo <7253alex@gmail.com>
2020-04-26 16:56:48 -0700

    heroku test

--- server/config/key.js
+++ server/config/key.js
@@ -1,7 +1,7 @@
 // if in production mode, export production file module
 // if not export development file module
 if (process.env.NODE_ENV === 'production') {
-    module.exports = required('./prod');
+    module.exports = require('./prod');
 }
 else {
     module.exports = require('./dev');
